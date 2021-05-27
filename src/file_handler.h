65a29d1e3e34a64928f8b943b75ff4015e97a807
martintc <kj4ntv@gmail.com>
2021-05-27 15:39:16 -0700

    Added and removed a function
    also included stdio

--- src/file_handler.h
+++ src/file_handler.h
@@ -1,6 +1,8 @@
 #ifndef FILE_HANDLER
 #define FILE_HANDLER
 
+#include <stdio.h>
+
 // create full path from directory path and file
 char* make_full_path(char *path, char *file);
 
@@ -8,6 +10,8 @@ char* make_full_path(char *path, char *file);
 int check_existence(char *path);
 
 // return file contens
-FILE* file_contents(char *path);
+//FILE* file_contents(char *path);
+
+FILE* get_file(char* path);
 
 #endif
